2caf85bb1c7f8dc278e2166c40933498cc85f410
Nicoli Cogliatti <115047865+nickcoglia@users.noreply.github.com>
2024-04-23 13:11:05 -0300

    Delete Sweet-Cat-main directory

--- Sweet-Cat-main/js/script.js
+++
@@ -1,5 +0,0 @@
-// Adicione um ouvinte de evento para o clique no botão "Jogar"
-document.getElementById("playButton").addEventListener("click", function() {
-    // Recarrega a página para exibir o jogo
-    window.location.href = "jogo.html";
-});
